842a73a8b952c36db65da902c8a434962fe0831c
Eugene Bang <79890348+eugenebang@users.noreply.github.com>
2022-12-05 00:14:09 +0900

    create utils.py

---
+++ DREAMwalk/utils.py
@@ -0,0 +1,24 @@
+import networkx as nx
+
+def read_graph(edgeList,weighted=True, directed=False,delimiter='\t'):
+    '''
+    Reads the input network in networkx.
+    '''
+    if weighted:
+        G = nx.read_edgelist(edgeList, nodetype=str, 
+                             data=(('type',int),('weight',float),('id',int)), 
+                             create_using=nx.MultiDiGraph(),
+                            delimiter=delimiter)
+    else:
+        G = nx.read_edgelist(edgeList, nodetype=str,data=(('type',int)), 
+                             create_using=nx.MultiDiGraph(),
+                            delimiter=delimiter)
+        for edge in G.edges():
+            edge=G[edge[0]][edge[1]]
+            for i in range(len(edge)):
+                edge[i]['weight'] = 1.0
+
+    if not directed:
+        G = G.to_undirected()
+
+    return G
