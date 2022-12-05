f9eb4af646a3bb84e5c6005311c5a55b4e143692
Eugene Bang <79890348+eugenebang@users.noreply.github.com>
2022-12-05 12:34:46 +0900

    fix bugs for generate_embeddings.py

--- DREAMwalk/generate_embeddings.py
+++ DREAMwalk/generate_embeddings.py
@@ -18,7 +18,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--network_file', type=str, required=True)
     
-    parser.add_argument('--sim_network_file', type=str, default=False)
+    parser.add_argument('--sim_network_file', type=str, default='')
     parser.add_argument('--output_file', type=str, default='embedding_file.pkl')
     parser.add_argument('--tp_factor', type=float, default=0.5)
     parser.add_argument('--weighted', type=bool, default=True)    
@@ -31,6 +31,7 @@ def parse_args():
                        help='if default, set to all available cpu count')
     parser.add_argument('--net_delimiter',type=str,default='\t',
                        help='delimiter of networks file; default = tab')
+
     return parser.parse_args()
 
 def generate_DREAMwalk_paths(G, G_sim, num_walks, walk_length,tp_factor, workers):
@@ -80,13 +81,13 @@ def _network_traverse(cur,G):
                 break
     return next    
 
-def _teleport_operation(cur,G):
-    cur_nbrs = sorted(G.neighbors(cur))
+def _teleport_operation(cur,G_sim):
+    cur_nbrs = sorted(G_sim.neighbors(cur))
     random.shuffle(cur_nbrs)
     selected_nbrs=[]
     distance_sum=0
     for nbr in cur_nbrs:
-        nbr_links = G[cur][nbr]
+        nbr_links = G_sim[cur][nbr]
         for i in nbr_links:
             nbr_link_weight = nbr_links[i]['weight']
             distance_sum += nbr_link_weight
@@ -97,7 +98,7 @@ def _teleport_operation(cur,G):
     threshold = 0
 
     for nbr in set(selected_nbrs):
-        nbr_links = G[cur][nbr]
+        nbr_links = G_sim[cur][nbr]
         for i in nbr_links:
             nbr_link_weight = nbr_links[i]['weight']
             threshold += nbr_link_weight
@@ -110,44 +111,63 @@ def _DREAMwalker(start_node, G, G_sim, walk_length,tp_factor):
     walk = [start_node]
     while len(walk) < walk_length:
         cur = walk[-1]
-        if np.random.rand() < tp_factor:
+        if (cur in G_sim.nodes()) & (np.random.rand() < tp_factor):
             next_node=_teleport_operation(cur,G_sim)
-            if not next_node:  # node not in G_sim
-                next_node=_network_traverse(cur,G)                
+#             if not next_node:  # node not in G_sim
+#                 next_node=_network_traverse(cur,G)                
         else:
             next_node=_network_traverse(cur,G)
         walk.append(next_node)
     return walk
 
-if __name__ == '__main__':
-    args=parse_args()
+def save_embedding_files(netf:str, sim_netf:str, outputf:str, weighted:bool, 
+                         directed:bool, tp_factor:float, num_walks:int, walk_length:int, 
+                         workers:int, dimension:int, window_size:int,
+                         net_delimiter:str='\t'):
     print('Reading network files...')
-    G=read_graph(args.network_file,
-                weighted=args.weighted,
-                directed=args.directed,
-                delimiter=args.net_delimiter)
-    if args.sim_network_file:
-        G_sim=read_graph(args.sim_network_file,
+    G=read_graph(netf,weighted=weighted,directed=directed,
+                 delimiter=net_delimiter)
+    if sim_netf:
+        G_sim=read_graph(sim_netf,
                         weighted=True,
                         directed=False)
-        tp_factor=args.tp_factor
     else:
-        G_sim=None
+        G_sim=nx.empty_graph()
         print('No input similarity file detected')
         tp_factor=0
-    walks=generate_DREAMwalk_paths(G,G_sim,args.num_walks,args.walk_length,
-                                  tp_factor, args.workers)
-    print('END of path generation')
+    
+    print('Generating paths...')
+    walks=generate_DREAMwalk_paths(G,G_sim,num_walks,walk_length,
+                                  tp_factor, workers)
     
     print('Generating node embeddings...')
-    model = Word2Vec(walks, vector_size = args.dimension, window= args.window_size, 
-                     min_count=0, sg=1, workers= args.workers)
+    model = Word2Vec(walks, vector_size=dimension, window=window_size, 
+                     min_count=0, sg=1, workers=workers)
     node_ids=model.wv.index_to_key
     node_embeddings=model.wv.vectors
 
     model_embeddings={}
     for i,idx in enumerate(node_ids):
         model_embeddings[idx]=node_embeddings[i]
-    with open(output_file,'wb') as fw:
+    with open(outputf,'wb') as fw:
         pickle.dump(model_embeddings,fw)
-    print(f'Node embeddings saved in: {args.output_file}')
+
+    print(f'Node embeddings saved in: {outputf}')
+
+if __name__ == '__main__':
+    args=parse_args()
+    args={
+        'netf':args.network_file,
+        'sim_netf':args.sim_network_file,
+        'outputf':args.output_file,
+        'tp_factor':args.tp_factor,
+        'weighted':args.weighted,
+        'directed':args.directed,
+        'num_walks':args.num_walks,
+        'walk_length':args.walk_length,
+        'dimension':args.dimension,
+        'window_size':args.window_size,
+        'workers':args.workers,
+        'net_delimiter':args.net_delimiter
+    }
+    save_embedding_files(**args)
