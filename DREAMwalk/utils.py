98e70c197c3700f5bd21b3101f557414144af7a6
Eugene Bang <79890348+eugenebang@users.noreply.github.com>
2022-12-05 12:47:45 +0900

    update utils.py

--- DREAMwalk/utils.py
+++ DREAMwalk/utils.py
@@ -1,4 +1,8 @@
+import os
+import random
+import numpy as np
 import networkx as nx
+import torch
 
 def read_graph(edgeList,weighted=True, directed=False,delimiter='\t'):
     '''
@@ -22,3 +26,14 @@ def read_graph(edgeList,weighted=True, directed=False,delimiter='\t'):
         G = G.to_undirected()
 
     return G
+
+def set_seed(seed=42):
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    print(f'random seed with {seed}')
