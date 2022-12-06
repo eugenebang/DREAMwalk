76c7f1ec7009ce38beb4e26e0591a66a76e3f5f2
Eugene Bang <79890348+eugenebang@users.noreply.github.com>
2022-12-06 21:48:45 +0900

    update utils.py

--- DREAMwalk/utils.py
+++ DREAMwalk/utils.py
@@ -1,8 +1,9 @@
 import os
+import torch
 import random
 import numpy as np
 import networkx as nx
-import torch
+
 
 def read_graph(edgeList,weighted=True, directed=False,delimiter='\t'):
     '''
@@ -37,3 +38,39 @@ def set_seed(seed=42):
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
     print(f'random seed with {seed}')
+    
+class EarlyStopper:
+    """Stop training when validation loss shows no improvement throughout patience epochs"""
+    def __init__(self, patience=7, printfunc=print,verbose=False, delta=0, path='checkpoint.pt'):
+        self.printfunc=printfunc
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+        self.delta = delta
+        self.path = path
+
+    def __call__(self, val_loss, model):
+        score = -val_loss
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            if self.verbose:
+                self.printfunc(f'EarlyStopping counter: {self.counter}/{self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model):
+        '''saves model when validation loss is decreased'''
+        if self.verbose:
+            self.printfunc(f'Validation loss decreased ({self.val_loss_min:.4f} --> {val_loss:.4f}).  Saving model ...')
+        torch.save(model.state_dict(), self.path)
+        self.val_loss_min = val_loss
